36fee1403530f69c313d8a91585b70bd78672389
Jhon Santana <jhonsstn@gmail.com>
2022-01-17 18:35:12 -0300

    corrigidos erros de lint

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,14 +33,12 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {
-  return {
-    sum: number1 + number2,
-    mult: number1 * number2,
-    div: Math.floor(number1 / number2),
-    sub: number1 - number2,
-  };
-};
+const calculator = (number1, number2) => ({
+  sum: number1 + number2,
+  mult: number1 * number2,
+  div: Math.floor(number1 / number2),
+  sub: number1 - number2,
+});
 
 const arrayGenerator = (type, object) => {
   switch (type) {
@@ -50,6 +48,7 @@ const arrayGenerator = (type, object) => {
       return Object.values(object);
     case 'entries':
       return Object.entries(object);
+    default:
   }
 };
 
